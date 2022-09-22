01f944717cfa28ef6e12a9c688bf4dc08c887f71
Alex Fisher <92438650+afisher3578@users.noreply.github.com>
2022-09-22 17:13:14 +0200

    Fix Instagram blinking (#46)
    * Fix blinking with new href filter
    
    * Update declaration history
    
    * Update removeTrackingIDs filters

--- declarations/Instagram.filters.js
+++ declarations/Instagram.filters.js
@@ -6,23 +6,31 @@ export const replaceInfoImageWithEmoji = document => {
   });
 };
 
-export function removeTrackingIDs(document) {
-  document.querySelectorAll('a').forEach(el => {
-    const href = el.getAttribute('href');
-    const params = new URLSearchParams(href);
-
-    if (params.has('h')) {
-      params.set('h', 'removed');
-      el.setAttribute('href', params.toString());
-    }
-  });
-}
-
 export function cleanUrls(document) {
   Array.from(document.querySelectorAll('[href="#"]')).map(link => link.removeAttribute('href'));
   const links = document.querySelectorAll('[href*="https://l.instagram.com/?"],[href*="http://l.instagram.com/?"]');
 
   links.forEach(link => {
     link.href = decodeURIComponent(link.href.replace(/&h=\S*/, '').replace(/(\S*)\?u=(\S*)/, '$2'));
+  });
+}
+
+export function removeTrackingIDs(document) {
+  removeQueryParam(document, 'h');
+}
+
+export function removeTrackingIDsE(document) {
+  removeQueryParam(document, 'e');
+}
+
+function removeQueryParam(document, queryParam) {
+  document.querySelectorAll('a').forEach(el => {
+    const href = el.getAttribute('href');
+    const params = new URLSearchParams(href);
+
+    if (params.has(queryParam)) {
+      params.delete(queryParam);
+      el.setAttribute('href', params.toString());
+    }
   });
 }
