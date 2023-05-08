1b04ae9ed5a3fab93576bc1d2aa1492fe95a5646
Alex Fisher <92438650+afisher3578@users.noreply.github.com>
2023-05-08 10:46:33 +0200

    Clean dataset  (#115)
    * Clean LINE
    
    * Clean Facebook Terms of Service
    
    * Clean Quora Terms of Service
    
    * Clean WhatsApp Terms of Service
    
    * Clean WhatsApp Privacy Policy
    
    * Clean Parler Privacy Policy
    
    * Clean Snapchat Terms of Service
    
    * Clean Parler Terms of Service
    
    * Clean Pinterest Terms of Service
    
    * Clean Twitch Terms of Service
    
    * Clean Reddit Terms of Service
    
    * Clean Telegram Privacy Policy
    
    * Clean Reddit Privacy Policy
    
    * Clean YouTube Privacy Policy
    
    * Clean Facebook Privacy Policy
    
    * Redo FB tos
    
    * Clean Facebook Data Processor Agreement
    
    * Clean Facebook Live Policy
    
    * Clean Facebook Law Enforcement Guidelines
    
    * Clean Parler Community Guidelines
    
    * Clean Pinterest Privacy Policy
    
    * Clean Pinterest Community Guidelines
    
    * Clean Reddit Live Policy
    
    * Clean Reddit Quality Guidelines
    
    * Clean Snapchat Privacy Policy
    
    * Clean Snapchat Trackers Policy
    
    * Clean Snapchat Community Guidelines
    
    * Clean Tumblr Privacy Policy
    
    * Clean Tumblr Terms of Service
    
    * Clean Telegram Terms of Service
    
    * Clean Telegram Developer Terms
    
    * Clean Twitch Privacy Policy
    
    * Clean WhatsApp Trackers Policy
    
    * Clean Twitch Community Guidelines
    
    * Clean Quora Privacy Policy
    
    * Clean Spotify Privacy Policy
    
    * Clean Spotify Terms of Service
    
    * Clean Spotify Acceptable Use Policy
    
    * Clean Spotify Community Guidelines
    
    * Clean WeChat Terms of Service
    
    * Clean WeChat Privacy Policy
    
    * Clean WeChat Acceptable Use Policy
    
    * Clean YouTube Terms of Service
    
    * Clean YouTube Developer Terms
    
    * Clean YouTube Copyright Claims Policy
    
    * Clean LinkedIn Privacy Policy
    
    * Clean TikTok Terms of Service
    
    * Clean TikTok Privacy Policy
    
    * Clean TikTok Law Enforcement Guidelines
    
    * Clean TikTok Copyright Claims Policy
    
    * Clean TikTok In-App Purchases Policy
    
    * Clean TikTok Trackers Policy
    
    * Clean TikTok Community Guidelines
    
    * Clean Twitter Terms of Service
    
    * Clean Twitter Privacy Policy
    
    * Clean Twitter Developer Terms
    
    * Clean Twitter Law Enforcement Guidelines
    
    * Clean Twitter Trackers Policy
    
    * Clean Twitter Data Processor Agreement
    
    * Clean Twitter Deceased Users
    
    * Clean Twitter Copyright Claims Policy
    
    * Clean Twitter Community Guidelines
    
    * Clean Facebook Community Guidelines
    
    * Clean LinkedIn Trackers Policy
    
    * Clean LinkedIn Terms of Service
    
    * Clean LinkedIn Developer Terms
    
    * Clean LinkedIn Community Guidelines
    
    * Clean YouTube Community Guidelines
    
    * Clean Quora Community Guidelines
    
    * Clean Reddit Community Guidelines
    
    * Clean Tumblr Community Guidelines
    
    * Clean Instagram Privacy Policy
    
    * Clean Instagram Terms of Service
    
    * Clean Instagram Law Enforcement Guidelines
    
    * Clean Instagram Developer Terms
    
    * Clean Instagram Community Guidelines
    
    * Clean LinkedIn Brand Guidelines
    
    * Fix wrong validuntil date causing crash of the cleaner
    
    * Clean Snapchat Privacy Policy
    
    * Clean Snapchat Trackers Policy
    
    * Clean Snapchat Community Guidelines
    
    * Clean TikTok Privacy Policy
    
    * Clean Twitch Privacy Policy
    
    * Clean Facebook Privacy Policy
    
    * Clean Instagram Privacy Policy
    
    * clean LinkedIn
    
    * Clean Tumblr Privacy Policy
    
    * Fix formatting in Facebook
    
    * Fix formating FB, Linkdin, Twitter
    
    * Update Facebook Cookies Policy Selector
    
    * Remove filter from Facebook Cookies Policy
    
    * "executeClientScripts" for Trackers Policy
    
    ---------
    
    Co-authored-by: Martin Ratinaud <martinratinaud@gmail.com>

--- declarations/LinkedIn.filters.js
+++ declarations/LinkedIn.filters.js
@@ -8,3 +8,29 @@ export function removeDownloadedForUseLinkExtension(document) {
     }
   });
 }
+
+export function removeTrackingIDs(document) {
+  removeQueryParam(document, '?trk=microsites-frontend_legal_privacy-policy&lang=en');
+}
+
+function removeQueryParam(document, queryParam) {
+  document.querySelectorAll('a').forEach(el => {
+    const href = el.getAttribute('href');
+    const params = new URLSearchParams(href);
+
+    if (params.has(queryParam)) {
+      params.delete(queryParam);
+      el.setAttribute('href', params.toString());
+    }
+  });
+}
+
+export function removeLinks(document) {
+  const elements = document.querySelectorAll('a');
+
+  elements.forEach(element => {
+    if (element.hasAttribute('href')) {
+      element.removeAttribute('href');
+    }
+  });
+}
